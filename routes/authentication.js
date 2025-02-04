56198da0482bf2a848a08c6a1fbf470037a95aef
Ismael110599 <castroismael571@gmail.com>
2025-02-03 21:57:19 -0500

    commit

--- routes/authentication.js
+++ routes/authentication.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { createAuthentication, getAllAuthentications, getAuthenticationById, updateAuthentication, deleteAuthentication } = require('../controllers/authenControllers');
 
-router.post('/auth', createAuthentication);
+router.post('/register', createAuthentication);
 router.post('/auth', getAllAuthentications);
 router.post('/auth/:id', getAuthenticationById);
 router.put('/auth/:id', updateAuthentication);
