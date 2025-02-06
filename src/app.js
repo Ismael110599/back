c2ad4ffc60ee09b02b1a61923c079b0ae1a2f7fa
Ismael110599 <castroismael571@gmail.com>
2025-02-06 00:44:04 -0500

    commit

--- src/app.js
+++ src/app.js
@@ -3,6 +3,9 @@ const cors = require('cors');
 const { requestLogger } = require('./middleware/logger');
 const connectDB = require('./config/db');
 
+const cors = require('cors');
+const morgan = require('morgan');
+
 const authRoutes = require('./routes/authRoutes');
 const userRoutes = require('./routes/userRoutes');
 const caretakerRoutes = require('./routes/caretakerRoutes')
@@ -13,6 +16,17 @@ require('dotenv').config();
 connectDB();
 
 const app = express();
+
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}))
+
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms'));
+
+
 
 app.use(cors());
 app.use(express.json());
