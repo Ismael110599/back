a13db526acdbc52adaa68f06c316faa59a07e190
RicardoRios <72361340+RicardoRios07@users.noreply.github.com>
2025-03-06 12:15:01 -0500

    Update MongoDB connection to use environment variable for password and add connection check

--- src/config/db.js
+++ src/config/db.js
@@ -1,13 +1,29 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
+    if (mongoose.connection.readyState !== 0) {
+        console.log('Ya existe una conexión activa a MongoDB.');
+        return;
+    }
+
+    console.log('Conectando a MongoDB...');
+
     try {
-        await mongoose.connect('mongodb://localhost:27017/petcat');
-        console.log('MongoDB conectado');
+
+        await mongoose.connect(`mongodb+srv://admin:${process.env.MONGO_PWD}@cluster0.di8sg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            dbName: 'petcat', 
+        });
+
+
+        console.log('MongoDB Connected');
+
     } catch (error) {
-        console.error('Error de conexión a MongoDB:', error);
+        console.error('MongoDB connection failed:', error);
         process.exit(1);
     }
 };
 
 module.exports = connectDB;
+connectDB();
